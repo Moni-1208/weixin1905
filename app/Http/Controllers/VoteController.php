fc609de2f77fe9fe2bbaaa1f2d08bd1ec5df9d39
baixue <baixue@qq.com>
2019-12-19 07:58:07 +0800

    投票7

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -15,20 +15,39 @@ class VoteController extends Controller
     	$code=$_GET['code'];
     	// 获取access_token
     	$data=$this->getAccessToken($code);
-    	// 获取用户信息 xxx
+    	// print_r($data);die;
+    	// 获取用户信息
     	$user_info=$this->getUserInfo($data['access_token'],$data['openid']);
     	// 处理业务逻辑  
     	// TODO 判断是否已经投过  使用redis 集合 或 有序集合
     	$openid=$user_info['openid'];
-    	$key='s:vote:xiaobai';
-    	Redis::sadd($key,$openid);
+    	$key='ss:vote:xiaobai';
 
-    	$members=Redis::members($key);
-    	echo'<pre>';print_r($numbers);echo '</pre>'; // 获取所有投票人的openid
-    	$total=Redis::scard($key);  // 统计投票总人数
+    	// 判断是否已经投过票
+    	if(Redis::zrank($key,$user_info['openid'])){
+    		echo "已经投过票了";
+    	}else{
+	    	Redis::zadd($key,time(),$openid);
+    	}
+
+    	$total=Redis::zCard($key); // 获取总人数
+    	echo '<pre>';print_r($total);echo '</pre>';
+    	$members=Redis::zRRang($key,0,-1,true);
+    	echo'<pre>';print_r($numbers);echo '</pre>'; // die; // 获取所有投票人的openid
+    	// foreach 是干什么用的？？？
+    	foreach ($numbers as $k => $v) {
+    		echo "用户：",$k.'投票时间'.date('Y-m-d H:i:s',$v);
+    	}
+    	$total=Redis::Scard($key);  // 统计投票总人数
     	echo "投票总人数：".$total;
     	echo "<hr>";
     	echo '<pre>';print_r($number);echo '</pre>';
+
+    	// 测试代码
+    	// $redis_key='vote';
+    	// $number = Redis::incr($redis_key); // incr是increment的缩写 自增添加的意思
+    	// echo "投票成功,当前票数".$number;
+
     }
 
     /**
