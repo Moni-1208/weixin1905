f94f91ea0d3dcc78192e6e7cb7caa3cc335ceba2
baixue <baixue@qq.com>
2019-12-21 10:14:41 +0800

    商品管理 后台

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -14,4 +14,7 @@ Route::group([
 
     $router->resource('users', WxUserController::class);
 
+    
+    $router->resource('goods', GoodsController::class);   // 商品管理
+
 });
---
+++ app/Model/GoodsModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GoodsModel extends Model
+{
+    // 数据库名称
+    protected $table = 'p_goods';
+}
